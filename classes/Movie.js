7dd3c734c0ff4c9456ec11b5d5df2e99ffdbbe81
SkyAisha <aisha.adia@sky.uk>
2023-10-24 14:52:36 +0100

    completed workshop task

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,26 @@
 // import the Media class:
-
+const Media = require(".//Media");
 // create your Movie class:
+class Movie extends Media {
+  constructor(title, year, genre, director, duration, rating) {
+    super(title, year, genre);
+    this.director = director;
+    this.duration = duration;
+    this.rating = rating;
+  }
 
+  summary() {
+    return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Rating: ${this.rating}`;
+  }
+
+  static longestMovie(movies) {
+    if (movies.length === 0) {
+      return null;
+    }
+    return movies.reduce((longest, current) =>
+      current.duration > longest.duration ? current : longest
+    );
+  }
+}
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,28 @@
 // import the Media class:
-
+const Media = require(".//Media");
 // create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;
+  }
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
 
+  static shortestAlbum(songs) {
+    let shortestsong = [];
+    let shortestLength = 10000;
+
+    for (const song of songs) {
+      if (song.length < shortestLength) {
+        shortestLength = song.length;
+        shortestsong = song;
+      }
+    }
+    return shortestsong;
+  }
+}
 // don't change below
 module.exports = Music;
